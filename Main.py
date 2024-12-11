0321f30be9140d98a32c17898d09b46829be48ab
Guilherme <gmoreira0101@gmail.com>
2024-12-11 11:29:33 -0300

    Commit com a tela do jogo

--- Main.py
+++ Main.py
@@ -1,10 +1,20 @@
-from Settings import *
+from settings import *
 from sys import exit
+
+
+from game import Game
 
 class Main:
     def __init__(self):
+        
+        #General
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption('Tetris')
+
+        #components
+        self.game = Game()
     
     def run(self):
         while True:
@@ -12,8 +22,14 @@ class Main:
                 if event.type  == pygame.QUIT:
                     pygame.quit()
                     exit()
+            
+            #Display
             self.display_surface.fill(GRAY)
+            self.game.run()
+             
+            #updating the game
             pygame.display.update()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     main = Main()
