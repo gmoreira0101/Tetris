f1c4a8664c9ba9ce60d8b862f295267bb4eee181
Guilherme <gmoreira0101@gmail.com>
2024-12-11 15:52:38 -0300

    Não lembro oq eu mudei pra ser sincero mas tem o link do yt

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Tetris
 Desafio da cubos de python
+
+Link para estudo:
+https://www.youtube.com/watch?v=x8x76XickSY&list=PL4cUxeGkcC9iurLoO9Mu7GqsKlxEXcf8m&index=4
--- game.py
+++ game.py
@@ -6,15 +6,20 @@ class Game:
         #general
         self.surface = pygame.Surface((GAME_WIDTH,GAME_HEIGHT))
         self.display_surface = pygame.display.get_surface()
+
+        #Lines
+        self.line_surface = self.surface.copy()
     
     def draw_grid(self):
         for col in range(1, COLUMNS):
             x = col * CELL_SIZE
-            pygame.draw.line(self.surface, LINE_COLOR, (x, 0), (x,self.surface.get_height()), 1 )
+            pygame.draw.line(self.line_surface, LINE_COLOR, (x, 0), (x,self.surface.get_height()), 1 )
         
         for line in range(1 , ROWS):
             y = line * CELL_SIZE
-            pygame.draw.line(self.surface, LINE_COLOR, (0, y), (self.surface.get_width(), y), 1)
+            pygame.draw.line(self.line_surface, LINE_COLOR, (0, y), (self.surface.get_width(), y), 1)
+        
+        self.surface.blit(self.line_surface, (0,0))
 
     def run(self):
         self.draw_grid()
