314b961884f43f46ed9f4dee1fcfd852d215e194
Guilherme Moreira <gmoreira0101@gmail.com>
2024-12-12 11:19:55 -0300

    Commit com as peças e grid mais bonitinho

--- Settings.py
+++ Settings.py
@@ -16,6 +16,33 @@ PADDING = 20
 WINDOW_WIDTH = GAME_WIDTH + SIDEBAR_WIDTH + PADDING * 3
 WINDOW_HEIGHT = GAME_HEIGHT + PADDING * 2
 
+#game behaviour
+UPDATE_START_SPEED = 800
+MOVE_WAIT_TIME = 200
+ROTATE_WAIT_TIME = 200
+BLOCK_OFFSET = pygame.Vector2(COLUMNS // 2, 5)
+
 #Cores
-GRAY = '#1C1C1C'
-LINE_COLOR = '#FFFFFF'
+GRAY = (28, 28, 28)           # '#1C1C1C'
+LINE_COLOR = (255, 255, 255)  # '#FFFFFF'
+RED = (229, 27, 32)           # '#e51b20'
+PURPLE = (123, 33, 127)       # '#7b217f'
+CYAN = (108, 198, 217)        # '#6cc6d9'
+ORANGE = (240, 126, 19)       # '#f07e13'
+BLUE = (32, 75, 155)          # '#204b9b'
+YELLOW = (241, 230, 13)       # '#f1e60d'
+GREEN = (101, 179, 46)        # '#65b32e'
+
+
+#Shapes
+TETROMINOS = {
+    'T' : {'shape': [(0,0), (-1,0), (1,0), (0,-1)], 'color': PURPLE},
+    'O' : {'shape': [(0,0), (0,-1), (1,0), (1,-1)], 'color': YELLOW},
+    'J' : {'shape': [(0,0), (0,-1), (0,1), (-1,1)], 'color': BLUE},
+    'L' : {'shape': [(0,0), (0,-1), (0,1), (1,1)], 'color': ORANGE},
+    'I' : {'shape': [(0,0), (0,-1), (0,-2), (0,1)], 'color': CYAN},
+    'S' : {'shape': [(0,0), (-1,0), (0,-1), (1,-1)], 'color': GREEN},
+    'Z' : {'shape': [(0,0), (1,0), (0,-1), (-1,-1)], 'color': RED}
+}
+
+SCORE_DATA = {1: 40, 2: 100, 3: 300, 4:1200}
--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from settings import *
+from random import choice
 
 class Game:
     def __init__(self):
@@ -6,10 +7,18 @@ class Game:
         #general
         self.surface = pygame.Surface((GAME_WIDTH,GAME_HEIGHT))
         self.display_surface = pygame.display.get_surface()
+        self.sprites = pygame.sprite.Group()
+        self.rect = self.surface.get_rect(topleft = (PADDING, PADDING))
 
         #Lines
         self.line_surface = self.surface.copy()
-    
+        self.line_surface.fill((0,255,0))
+        self.line_surface.set_colorkey((0,255,0))
+        self.line_surface.set_alpha(120)
+
+        # tetromino
+        self.tetrmino = Tetronimo(choice(list(TETROMINOS.keys())), self.sprites)
+
     def draw_grid(self):
         for col in range(1, COLUMNS):
             x = col * CELL_SIZE
@@ -22,5 +31,33 @@ class Game:
         self.surface.blit(self.line_surface, (0,0))
 
     def run(self):
+
+        #drawing
+        self.surface.fill(GRAY)
+        self.sprites.draw(self.surface)
         self.draw_grid()
         self.display_surface.blit(self.surface, (PADDING,PADDING))
+        pygame.draw.rect(self.display_surface, LINE_COLOR, self.rect, 2, 2)
+
+class Tetronimo:
+    def __init__(self, shape, group):
+        #setup
+        self.block_positions = TETROMINOS[shape]['shape']
+        self.color = TETROMINOS[shape]['color']
+
+        #create blocks
+        self.blocks = [Block(group, pos, self.color) for pos in self.block_positions]
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, group, pos, color):
+        
+        #general
+        super().__init__(group)
+        self.image = pygame.Surface((CELL_SIZE, CELL_SIZE))
+        self.image.fill(color)
+       
+        #position
+        self.pos = pygame.Vector2(pos) + BLOCK_OFFSET
+        x = self.pos.x * CELL_SIZE
+        y = self.pos.y * CELL_SIZE
+        self.rect = self.image.get_rect(topleft = (x,y))
