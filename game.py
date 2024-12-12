66f54c2165c0e6528561e537d6da3b931d3a9e82
Guilherme Moreira <gmoreira0101@gmail.com>
2024-12-12 14:42:02 -0300

    Commit com os inputs do usuario

--- game.py
+++ game.py
@@ -23,7 +23,8 @@ class Game:
 
         #timer
         self.timers = {
-            'vertical move': Timer(UPDATE_START_SPEED, True, self.move_down)
+            'vertical move': Timer(UPDATE_START_SPEED, True, self.move_down),
+            'horizontal move' : Timer(MOVE_WAIT_TIME)
         }
         self.timers['vertical move'].activate()
     
@@ -45,9 +46,20 @@ class Game:
         
         self.surface.blit(self.line_surface, (0,0))
 
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if not self.timers['horizontal move'].active:
+            if keys[pygame.K_LEFT]:
+                self.tetromino.move_horizontal(-1)
+                self.timers['horizontal move'].activate()
+            if keys[pygame.K_RIGHT]:
+                self.tetromino.move_horizontal(1)
+                self.timers['horizontal move'].activate()
+    
     def run(self):
 
         #update
+        self.input()
         self.timer_update()
         self.sprites.update()
 
@@ -71,6 +83,10 @@ class Tetronimo:
     def move_down(self):
         for blocks in self.blocks:
             blocks.pos.y += 1
+    
+    def move_horizontal(self, amount):
+        for blocks in self.blocks:
+            blocks.pos.x += amount
 
 class Block(pygame.sprite.Sprite):
     def __init__(self, group, pos, color):
